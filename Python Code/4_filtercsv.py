1d0a0593b195095b3030cf5dfe59ca3b17ad80da
ruchira0103 <62032648+ruchira0103@users.noreply.github.com>
2020-08-12 01:32:21 +0100

    Create 4_filtercsv.py

---
+++ Python Code/4_filtercsv.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+"""
+-----------------------------------------------------------------------------------------------
+File:		  filtercsv.py
+Program:	filtercsv
+Version:	3.0
+Created:	12 Aug 2020
+Function:	Code below starts off with the IMGT data and filters it down to required resolution
+          and then continues with comparing the outfile with the PyIgClassify list and then 
+          uses that list to download the required list of interactions from the PDBSum website. 
+Author: 	Ruchira Sachdeva 
+------------------------------------------------------------------------------------------------
+"""
+#***********************************************************************************************
+#Import Libraries
+import pandas as pd
+import csv
+import re
+import zipfile
+import requests
+
+#***********************************************************************************************#
+        
+def filter_csv(infile):
+    """Function below allows filtering of IMGT data by capturing PDBs with Resolution < 2.5 A,
+    capitalizing the IMGT ID column and saving to new CSV file"""
+    with open(infile, 'r') as csvfile:
+        df = pd.read_csv(csvfile, sep='\t', engine='python', delimiter = ',')
+        df = df[df['Resolution'] <= 2.5]
+        df['IMGT entry ID'] = df['IMGT entry ID'].str.upper() 
+        compression_opts = dict(method='zip',archive_name='filtered_IMGT.csv') #save to zip file
+        df.to_csv('out.zip', index=False, compression=compression_opts) 
+        zip = zipfile.ZipFile('out.zip') #extract file here
+        zip.extractall()
+
+filter_csv('Ig-Ag Complex.csv')
+
+def matchingPDB(infile1, infile2, outfile):
+    """Function allows to search PDB codes from one file to another and save in a new CSV file"""
+    PDBs_sliced = [] #create an empty array 
+    #entering PDB codes into the array below
+    with open(infile1, 'r') as f:
+        df = pd.read_csv(f)
+        PDBs = list(df['PDB'])
+        PDBs_sliced = [x[:-1] for x in PDBs] 
+        
+    with open(infile2, 'r') as f, open(outfile, 'w') as outfile:
+        writer = csv.DictWriter(outfile, fieldnames = ["Unnamed", "IMGT entry ID", "IMGT molecule name", "Species",
+                                                       "IMGT entry type", "IMGT receptor description", "Ligand(s)",
+                                                       "Experimental technique", "Resolution", "PDB release date"])
+        writer.writeheader()
+        for line in f:
+            if str(line.split(',')[1]) in PDBs_sliced:
+                outfile.write(line)
+
+matchingPDB('IGHV1-69_01 Data.csv', 'filtered_IMGT.csv', 'PDBsmatched.csv')
+
+def download_file(infile):
+    """Function below takes the PDB list from the earlier function of comparing PDBs with IMGT &
+    PyIgClassify data and downloads the relevant 'List of atom-atom interactions across
+    protein-protein interface' document for each PDB"""
+    PDB_searchcodes = []
+    df = pd.read_csv(infile, sep='\t', engine='python', delimiter = ',')
+    for entry in (df['IMGT entry ID'].str.lower()):
+        PDB_searchcodes = entry
+        url = "http://www.ebi.ac.uk/thornton-srv/databases/cgi-bin/pdbsum/GetIface.pl?pdb="+PDB_searchcodes + '&chain1=H&chain2=L'
+        req = requests.get(url)
+        filename = req.url[url.rfind('?')+5:-18]
+        with open(filename, 'wb') as f:
+            for chunk in req.iter_content(chunk_size=8192):
+                if chunk:
+                    f.write(chunk)
+                    
+download_file('PDBsmatched.csv')
+
+def PDB_bonds(infile, outfile):
+    with open(infile,'r') as infile, open(outfile, 'w') as outfile:
+        copy = False
+        for line in infile:
+            if line.strip() == "Hydrogen bonds":
+                copy = True
+                continue
+            elif line.strip() == "Non-bonded contacts":
+                copy = False
+                continue
+            elif copy:
+                outfile.write(line)  
+
+PDB_bonds('5alb.txt', '5alb_bonds.csv')
