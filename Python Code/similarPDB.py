e2032713639b21b404f19d23dea4fcf52f418f6f
ruchira0103 <62032648+ruchira0103@users.noreply.github.com>
2020-09-04 20:25:52 +0100

    Update and rename 1_similarPDB.py to similarPDB.py

--- Python Code/similarPDB.py
+++ Python Code/similarPDB.py
@@ -2,7 +2,7 @@
 
 """
 -----------------------------------------------------------------------------------------------
-File:		similarPDB.py
+File:	similarPDB.py
 Program:	similarPDB
 Version:	3.0
 Created:	28 May 2020
@@ -18,10 +18,15 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import re
+
 #***********************************************************************************************#
 
+#Function below allows the user to input a filename and search the relevant CDR1/CDR2/CDR3 sequences
+#within the IGHV1-69 germline sequence and create a plot to showcase similarities and differences'
+
 def func(filename):
-    """Function below allows the user to input a filename and search the relevant CDR1/CDR2/CDR3 sequences within the IGHV1-69 germline sequence"""
+    """Function below allows the user to input a filename and search the relevant
+    CDR1/CDR2/CDR3 sequences within the IGHV1-69 germline sequence"""
     seq = 'MDWTWRFLFVVAAATGVQSQVQLVQSGAEVKKPGSSVKVSCKASGGTFSSYAISWVRQAPGQGLEWMGGIIPIFGTANYAQKFQGRVTITADKSTSTAYMELSSLRSEDTAVYYCAR'
     pattern = pd.read_csv(filename)
     df = pd.read_csv(filename)
@@ -38,19 +43,24 @@ def func(filename):
       else:
           continue
     print(wordcount)
-    print(CDR1)
+    #print(CDR1)
+          
 func("IGHV1-69_01 Data.csv")
 func("IGHV1-69_02 Data.csv")
-
+func("IGHV1-8_01 Data.csv")
+func("IGHV1-46_01 Data.csv")
+func("IGHV3-21_01 Data.csv")
 
 def dist_graph(filename):
-    """Function below plots a histogram to showcase differences between CDR1/CDR2/CDR3 sequences within the IGHV1-69 germline sequence"""
+    """Function below plots a histogram to showcase differences between CDR1/CDR2/CDR3
+    sequences within the IGHV1-69 germline sequence"""
     df = pd.read_csv(filename)
     CDR = ['CDR1 Len'], ['CDR2 Len', 'CDR3 Len']
     CDR1 = df['CDR1 Len'].values
     CDR2 = df['CDR2 Len'].values
     CDR3 = df['CDR3 Len'].values
     plt.hist((CDR1, CDR2, CDR3), bins = 5, edgecolor='black')
+    plt.xlim(8,25)
     plt.legend(['CDR1', 'CDR2', 'CDR3'])
     plt.title('CDR Length Distribution within ' +filename[0:8]+'*'+filename[9:11])
     plt.xlabel('CDR Sequence Length')
@@ -60,4 +70,8 @@ def dist_graph(filename):
 
 dist_graph("IGHV1-69_01 Data.csv")
 dist_graph("IGHV1-69_02 Data.csv")
+dist_graph("IGHV1-8_01 Data.csv")
+dist_graph("IGHV1-46_01 Data.csv")
+dist_graph("IGHV3-21_01 Data.csv")
+
 
