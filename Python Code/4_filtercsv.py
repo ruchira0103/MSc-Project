44a7804d5dddd095045b42382dc5c3ca4dc42ba4
ruchira0103 <62032648+ruchira0103@users.noreply.github.com>
2020-09-04 20:30:22 +0100

    Update 4_filtercsv.py

--- Python Code/4_filtercsv.py
+++ Python Code/4_filtercsv.py
@@ -2,13 +2,12 @@
 
 """
 -----------------------------------------------------------------------------------------------
-File:		  filtercsv.py
+File:	4_filtercsv.py
 Program:	filtercsv
 Version:	3.0
-Created:	12 Aug 2020
-Function:	Code below starts off with the IMGT data and filters it down to required resolution
-          and then continues with comparing the outfile with the PyIgClassify list and then 
-          uses that list to download the required list of interactions from the PDBSum website. 
+Created:	28 July 2020
+Function:	This code takes the CDR Sequences and filters, downloads and parses files within
+          the various IGHV germlines
 Author: 	Ruchira Sachdeva 
 ------------------------------------------------------------------------------------------------
 """
@@ -54,37 +53,92 @@ def matchingPDB(infile1, infile2, outfile):
             if str(line.split(',')[1]) in PDBs_sliced:
                 outfile.write(line)
 
-matchingPDB('IGHV1-69_01 Data.csv', 'filtered_IMGT.csv', 'PDBsmatched.csv')
+matchingPDB('IGHV1-69_01 Data.csv', 'filtered_IMGT.csv', 'PDBsmatched_V1-69_01.csv')
+matchingPDB('IGHV1-69_02 Data.csv', 'filtered_IMGT.csv', 'PDBsmatched_V1-69_02.csv')
+matchingPDB('IGHV1-8_01 Data.csv', 'filtered_IMGT.csv', 'PDBsmatched_V1-8_01.csv')
+matchingPDB('IGHV1-46_01 Data.csv', 'filtered_IMGT.csv', 'PDBsmatched_V1-46_01.csv')
+matchingPDB('IGHV3-21_01 Data.csv', 'filtered_IMGT.csv', 'PDBsmatched_V3-21_01.csv')
+
 
 def download_file(infile):
     """Function below takes the PDB list from the earlier function of comparing PDBs with IMGT &
     PyIgClassify data and downloads the relevant 'List of atom-atom interactions across
-    protein-protein interface' document for each PDB"""
+    protein-protein interface' document for each PDB and saves as CSV file"""
     PDB_searchcodes = []
     df = pd.read_csv(infile, sep='\t', engine='python', delimiter = ',')
     for entry in (df['IMGT entry ID'].str.lower()):
         PDB_searchcodes = entry
         url = "http://www.ebi.ac.uk/thornton-srv/databases/cgi-bin/pdbsum/GetIface.pl?pdb="+PDB_searchcodes + '&chain1=H&chain2=L'
         req = requests.get(url)
-        filename = req.url[url.rfind('?')+5:-18]
+        filename = req.url[url.rfind('?')+5:-18]+'.csv'
         with open(filename, 'wb') as f:
             for chunk in req.iter_content(chunk_size=8192):
                 if chunk:
                     f.write(chunk)
-                    
-download_file('PDBsmatched.csv')
-
-def PDB_bonds(infile, outfile):
-    with open(infile,'r') as infile, open(outfile, 'w') as outfile:
-        copy = False
-        for line in infile:
-            if line.strip() == "Hydrogen bonds":
-                copy = True
-                continue
-            elif line.strip() == "Non-bonded contacts":
+            outfile_name = 'out-' + filename
+            with open(filename,'r') as infile, open(outfile_name, 'w') as outfile:
                 copy = False
-                continue
-            elif copy:
-                outfile.write(line)  
+                for line in infile:
+                    #if line in "<":
+                        #infile = line.replace(char,'')
+                    if line.strip() == "Hydrogen bonds":
+                        copy = True
+                        continue
+                    elif line.strip() == "Non-bonded contacts":
+                        copy = False
+                        continue
+                    elif copy:
+                        outfile.write(line)
+                
 
-PDB_bonds('5alb.txt', '5alb_bonds.csv')
+download_file('PDBsmatched_V1-69_01.csv')
+download_file('PDBsmatched_V1-69_02.csv')
+download_file('PDBsmatched_V1-8_01.csv')
+download_file('PDBsmatched_V1-46_01.csv')
+download_file('PDBsmatched_V3-21_01.csv')
+
+CDR1 = range(0,30)
+CDR2 = range(30-50)
+CDR3 = range(50-100)
+def parsecsv(infile):
+    """Function below uses the csv files produced from earlier function to create a new
+    column to capture the residue symbols to create comparisons with logo plots"""
+    df = pd.read_csv(infile)
+    df['Res Symbol'] = df['Res name'].astype(str).str[0]
+    def  expert_level_check(num):
+        if 0<= num < 30:
+            return 'CDR1'
+        elif 31<= num < 50:
+            return 'CDR2'
+        else:
+            return 'CDR3'
+    df['CDR'] = df['Res no'].apply(expert_level_check)
+    new_rows = (df[['Res name','Res no','Res Symbol', 'CDR']])
+    print(new_rows)
+    with open('output.csv', 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        for row in new_rows.items():
+            csvwriter.writerow(row)
+
+
+"""IGHV1-69*01"""
+parsecsv('out-2cmr.csv')
+parsecsv('out-2fx7.csv')
+parsecsv('out-2fx8.csv')
+
+"""IGHV1-69*02"""
+parsecsv('out-1yym.csv')
+parsecsv('out-2dd8.csv')
+
+"""IGHV1-8*01"""
+parsecsv('out-3x3f.csv')
+parsecsv('out-4xmp.csv')
+
+"""IGHV1-46*01"""
+parsecsv('out-4lsp.csv')
+parsecsv('out-5f9o.csv')
+
+"""IGHV3-21*01"""
+parsecsv('out-6cxg.csv')
+parsecsv('out-6mu3.csv')
+
